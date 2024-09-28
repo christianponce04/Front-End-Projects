bde9fb240145682b72256e561d1f50a6f7ab6a25
Christian Ponce <christianponce04@gmail.com>
2024-09-28 23:40:45 +0800

    adding updates functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const nav = document.getElementById('nav_area');
 const mainLogo = document.getElementById('main_logo');
 const header = mainLogo.parentNode;
@@ -30,3 +29,37 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+// dropdown
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+// Select all elements by the class name
+const arrrow_down = document.getElementsByClassName('arrrow_down');
+
+// Loop through each div and add event listeners
+for (let i = 0; i < arrrow_down.length; i++) {
+    arrrow_down[i].addEventListener('click', function() {
+        // Toggle the 'clicked' class for the clicked div
+        arrrow_down[i].classList.toggle('clicked');
+    });
+}
+
